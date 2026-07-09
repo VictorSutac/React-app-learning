83ad5f6c88345ba3c74fd77641e5e9bfe1273fa3
VictorSutac <156675989+VictorSutac@users.noreply.github.com>
2026-07-09 16:54:44 +0300

    server brunch init

--- src/components/SearchImport/index.jsx
+++
@@ -1,1 +0,0 @@
-export {SearchImport} from './SearchImport';
--- src/helper/delayFn.js
+++
@@ -1,3 +0,0 @@
-export const delayFn = async (delay = 1000)=>{
-    return await new Promise((resolve) => setTimeout(resolve, delay)); 
-}
